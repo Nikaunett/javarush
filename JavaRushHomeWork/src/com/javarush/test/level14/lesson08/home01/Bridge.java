254da9fefac2e0def942ff3684469b7220ae572c
Nikaunett <oshe.vika@gmail.com>
2016-05-01 07:08:06 +0600

    15 level пустой

--- JavaRushHomeWork/src/com/javarush/test/level14/lesson08/home01/Bridge.java
+++
@@ -1,9 +0,0 @@
-package com.javarush.test.level14.lesson08.home01;
-
-/**
- * Created by oshev on 24.04.2016.
- */
-public interface Bridge
-{
-    int getCarsCount();
-}
--- JavaRushHomeWork/src/com/javarush/test/level14/lesson08/home05/CompItem.java
+++
@@ -1,9 +0,0 @@
-package com.javarush.test.level14.lesson08.home05;
-
-/**
- * Created by oshev on 26.04.2016.
- */
-public interface CompItem
-{
-    String getName();
-}
